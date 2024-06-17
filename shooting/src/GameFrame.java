a969df988b2c28a2122c2f4902a54309a395c57e
mizuki <zeal23418011@fsgcl.jp>
2024-06-17 11:08:33 +0900

    10.6

--- shooting/src/GameFrame.java
+++ shooting/src/GameFrame.java
@@ -14,7 +14,12 @@ while(true) {
 		PlayerBullet b=GameWorld.playerBullets.get(i);
 		b.draw(this);
 		b.move();
-		i++;
+	
+		if(b.y<0) {
+			GameWorld.playerBullets.remove(i);
+		}else {
+			i++;
+		}
 	}
 	sleep(0.03);
 }
