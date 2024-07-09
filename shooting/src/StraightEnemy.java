e015705ca741b164f8797626925d380d778a1efc
mizuki <zeal23418011@fsgcl.jp>
2024-07-09 10:13:32 +0900

    rensyu12

--- shooting/src/StraightEnemy.java
+++ shooting/src/StraightEnemy.java
@@ -1,6 +1,6 @@
 
 public class StraightEnemy extends Enemy{
-
+int life=5;
 	public StraightEnemy(double x, double y, double vx, double vy) {
 		super(x, y, vx, vy);
 		// TODO 自動生成されたコンストラクター・スタブ
@@ -11,4 +11,5 @@ public class StraightEnemy extends Enemy{
 		f.setColor(0, 0, 0);
 		f.fillRect(x+10, y+15, 10, 15);
 	}
+	
 }
